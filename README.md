c3a4262360e17179317e0cdfed13ffc7121208b6
nelson <nelson2014@qq.com>
2018-06-28 11:45:30 +0800

    添加md

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # MarqueeView
 新的里程表式垂直跑马灯，基于viewgroup的自定义控件
 
+## 效果图
+<img src=".images/20180628.gif" height="650"/>
--- app/src/main/java/com/libs/nelson/marqueeview/TestAdapter.kt
+++ app/src/main/java/com/libs/nelson/marqueeview/TestAdapter.kt
@@ -1,7 +1,6 @@
 package com.libs.nelson.marqueeview
 
 import android.support.v7.widget.RecyclerView
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
